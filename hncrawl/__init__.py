bb9c9398c034ab814abd2f648e80fe4d4a15221c
Jesse B. Hannah <jesse@jbhannah.net>
2019-11-04 10:30:46 -0700

    Asynchronously get comments for stories

--- hncrawl/__init__.py
+++ hncrawl/__init__.py
@@ -1,5 +1,50 @@
-from .hn import get_index_stories, get_story_comments
+from asyncio import Queue, create_task, gather, run, sleep
+
+from aiohttp import ClientSession
+
+from .hn import RateLimitError, get_index_stories
 
 
 def main():
-    stories = get_index_stories()
+    run(_main())
+
+
+async def _worker(i, queue, session):
+    while True:
+        try:
+            story = await queue.get()
+            print("Worker {} handling {}".format(i, story.id_))
+            await story.get_comments(session)
+        except RateLimitError as e:
+            print("Worker {} encountered error: {}".format(i, e.message))
+            await sleep(1)
+            await queue.put(story)
+        finally:
+            queue.task_done()
+            print("Worker {} done, {} in queue".format(i, queue.qsize()))
+
+
+async def _main(max_workers=10):
+    async with ClientSession() as session:
+        queue = Queue(maxsize=max_workers)
+        workers = [
+            create_task(_worker(i, queue, session)) for i in range(max_workers)
+        ]
+
+        stories = await get_index_stories(session)
+
+        for story in stories:
+            await queue.put(story)
+
+        print("Awaiting queue")
+        await queue.join()
+
+        for worker in workers:
+            worker.cancel()
+
+        print("Canceling workers")
+        await gather(*workers, return_exceptions=True)
+
+    for story in stories:
+        print(story)
+        print(story.top_words)
--- hncrawl/hn.py
+++ hncrawl/hn.py
@@ -1,15 +1,83 @@
+import re
+from collections import Counter
+from os import path
+
+from aiohttp import request
 from bs4 import BeautifulSoup
-from requests import get
 
-from .story import Story
+fail_message = "Sorry, we're not able to serve your requests this quickly."
+words = re.compile(r"[A-Za-z']+")
+
+with open(path.join(path.dirname(__file__), "stopwords-en.txt"), "r") as file:
+    stopwords = [s for s in map(str.strip, file.readlines())]
 
 
-def get_index_stories():
-    index = get("https://news.ycombinator.com/")
-    index_soup = BeautifulSoup(index.content, "html.parser")
+class RateLimitError(Exception):
+    def __init__(self, id_):
+        self.id_ = id_
+        self.message = "Rate limited when attempting to get comments for {}".format(
+            self.id_)
+
+
+class Story:
+    def __init__(self, id_=None, rank=None, title=None):
+        self.id_ = id_
+        self.rank = rank
+        self.title = title
+        self.comments = []
+        self.top_words = []
+
+    def __repr__(self):
+        return "{}. {} ({})".format(self.rank, self.title, self.id_)
+
+    async def get_comments(self, session):
+        self.comments = await get_story_comments(session, self.id_)
+        self._get_top_comment_words()
+
+    def _get_top_comment_words(self):
+        comment_words = map(str.lower, words.findall(" ".join(self.comments)))
+        c = Counter(filter(lambda w: w not in stopwords, comment_words))
+        self.top_words = c.most_common(5)
+
+
+async def get_index_stories(session):
+    index = await _get(session, "https://news.ycombinator.com/")
+    index_soup = BeautifulSoup(index, "html.parser")
+
     return [
         Story(id_=row['id'],
               rank=int(float(row.find("span", "rank").string)),
               title=row.find("a", "storylink").string)
         for row in index_soup.find_all("tr", "athing")
     ]
+
+
+async def get_story_comments(session, id_):
+    print("Getting comments for {}".format(id_))
+    story = await _get(session,
+                       "https://news.ycombinator.com/item?id={}".format(id_))
+    story_soup = BeautifulSoup(story, "html.parser")
+
+    comments = [
+        _sanitized_comment_string(comment)
+        for comment in story_soup.find_all("span", "commtext")
+    ]
+
+    if len(comments) > 0 or "".join(
+            s for s in story_soup.strings).find(fail_message) == -1:
+        print("Got {} comments for {}".format(len(comments), id_))
+        return comments
+
+    raise RateLimitError(id_)
+
+
+def _sanitized_comment_string(comment):
+    while comment.a:
+        comment.a.decompose()
+
+    return " ".join(string for string in comment.strings)
+
+
+async def _get(session, url):
+    response = await session.get(url)
+    return await response.text()
